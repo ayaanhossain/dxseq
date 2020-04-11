3b56811c210f138e9072717f01e0e35cce7eb324
Ayaan Hossain <ain.hoss07@gmail.com>
2020-04-10 22:07:42 -0400

    Commit Zero

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'dxseq',
+  packages = ['dxseq'],
+  version = '0.1',
+  license='MIT',
+  description = 'Massively Parallel Diagnostic Assay Design and Sequencing',
+  author = 'Ayaan Hossain',
+  author_email = '',
+  url = 'https://github.com/ayaanhossain/dxseq',
+  download_url = '',
+  keywords = [],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Data Analyst',
+    'Topic :: Experimental Design and Data Analysis :: Data Analysis',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
